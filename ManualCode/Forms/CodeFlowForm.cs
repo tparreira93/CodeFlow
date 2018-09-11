c038ee3dbda3addc8ebf96567e99e1307e761536
Thales Parreira <thalesvparreira@hotmail.com>
2018-09-11 10:02:28 +0100

    Fixed codeflowform opening.

--- ManualCode/Forms/CodeFlowForm.cs
+++ ManualCode/Forms/CodeFlowForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -10,27 +11,50 @@ namespace CodeFlow.Forms
     public abstract class CodeFlowForm : Form, ICodeFlowForm
     {
         private static Dictionary<Type, object> openForms = new Dictionary<Type, object>();
+        private object lockObject = new object();
+
+        private bool Lock()
+        {
+            bool lockWasTaken = false;
+            Monitor.Enter(lockObject, ref lockWasTaken);
+
+            return lockWasTaken;
+        }
+        private void ReleaseLock()
+        {
+            Monitor.Exit(lockObject);
+        }
 
         public void Open()
         {
-            Type t = this.GetType();
-            object obj = null;
-            if (openForms.ContainsKey(t))
-                obj = openForms[t];
-
-            Form form = obj as Form;
-            if (form != null)
-                form.Show();
-            else
+            if (Lock())
             {
-                openForms.Add(t, this);
-                this.Show();
+                Type t = this.GetType();
+                object obj = null;
+                if (openForms.ContainsKey(t))
+                    obj = openForms[t];
+
+                Form form = obj as Form;
+                if (form != null && !form.IsDisposed)
+                    form.Focus();
+                else
+                {
+                    openForms.Add(t, this);
+                    this.FormClosing += CloseForm;
+                    this.Show();
+                }
+                ReleaseLock();
             }
         }
 
-        public void CloseForm()
+        public void CloseForm(object sender, FormClosingEventArgs e)
         {
-
+            if(Lock())
+            {
+                if (openForms.ContainsKey(this.GetType()))
+                    openForms.Remove(this.GetType());
+                ReleaseLock();
+            }
         }
     }
 }
